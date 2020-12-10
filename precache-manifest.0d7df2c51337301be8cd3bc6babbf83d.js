afd5915dc72a1cc140bf7eec8428bf39edf3164f
Aditya2102 <46434644+Aditya2102@users.noreply.github.com>
2020-12-10 16:50:48 +0530

    Add files via upload

---
+++ precache-manifest.0d7df2c51337301be8cd3bc6babbf83d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96dfd23a4206752af4a6e4cd5a5d60a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f9c75bd43299fe74638",
+    "url": "/static/js/2.33c35699.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.33c35699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631b9a5289fe52647bf3",
+    "url": "/static/js/main.bdf386bf.chunk.js"
+  },
+  {
+    "revision": "4add2adcc1d007738af1",
+    "url": "/static/js/runtime-main.46cc9a21.js"
+  }
+]);
